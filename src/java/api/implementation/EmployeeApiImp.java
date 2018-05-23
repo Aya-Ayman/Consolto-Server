96ed0543e2f695a3bcd6b0a4651868a1104fd32d
hoda mohamed <eng.hodamohamed1@gmail.com>
2018-05-23 13:18:17 +0200

    hodachange3

--- src/java/api/implementation/EmployeeApiImp.java
+++ src/java/api/implementation/EmployeeApiImp.java
@@ -9,12 +9,15 @@ import SendMailImp.SendMail;
 import api.interfaces.EmployeeApiInt;
 import dao.Implementation.employee.EmployeeDaoImp;
 import dao.Interfaces.employee.EmployeeDaoInt;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import pojos.EmployeePojo;
 import pojos.ResponseMessage;
 import pojos.ResponseMessageWithId;
@@ -27,14 +30,22 @@ import pojos.ResponseMessageWithId;
 public class EmployeeApiImp implements EmployeeApiInt {
     @POST
     @Path("/login")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Override
-    public ResponseMessageWithId login(@FormParam("name")String mail,@FormParam("number")String password){
+    public ResponseMessageWithId login(JSONObject login){
         ResponseMessageWithId response = new ResponseMessageWithId();
         EmployeePojo emp;
+         String email=null;
+         String password=null;
+        try {
+            email = (String) login.get("mail");
+            password = (String) login.get("password");
+        } catch (JSONException ex) {
+            Logger.getLogger(EmployeeApiImp.class.getName()).log(Level.SEVERE, null, ex);
+        }
        EmployeeDaoInt empdao=new EmployeeDaoImp();
-       emp= empdao.retrieveByMailAndPassword(mail, password);
+       emp= empdao.retrieveByMailAndPassword(email, password);
        if (emp.getEmployeeId()== 0)
        {
            response.setMessage("user not found");
@@ -53,30 +64,35 @@ public class EmployeeApiImp implements EmployeeApiInt {
    
     @POST
     @Path("/forget")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Override
-    public ResponseMessage getEmp(@FormParam("name")String mail) {
-    
-    
-        ResponseMessage response = new ResponseMessage();
-        EmployeePojo emp;
-       EmployeeDaoInt empdao=new EmployeeDaoImp();
-       emp= empdao.retrieveByMail(mail);
-       if (emp.getEmployeeId()== 0)
-       {
-           response.setMessage("user not found");
-           response.setStatus(false);
-           response.setError("404");
-           return response;
-       }else
-               {
-           response.setMessage("Check Your Mail");
-           response.setStatus(true);
-           response.setError("200");
-           SendMail send = new SendMail();
-           send.sendmail(emp.getEmployeeEmail(),emp.getEmployeePassword());
-           return response;
-               }
+    public ResponseMessage getEmp(JSONObject mail) {
+      ResponseMessage response = new ResponseMessage();
+      EmployeePojo emp;
+            String email=null;
+        try {
+            email = (String) mail.get("mail");
+            
+        } catch (JSONException ex) {
+            Logger.getLogger(EmployeeApiImp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            EmployeeDaoInt empdao=new EmployeeDaoImp();
+            emp= empdao.retrieveByMail(email);
+            if (emp.getEmployeeId()== 0)
+            {
+                response.setMessage("user not found");
+                response.setStatus(false);
+                response.setError("404");
+                return response;
+            }else
+            {
+                response.setMessage("Check Your Mail");
+                response.setStatus(true);
+                response.setError("200");
+                SendMail send = new SendMail();
+                send.sendmail(emp.getEmployeeEmail(),emp.getEmployeePassword());
+                return response;
+            }
     }
 }
